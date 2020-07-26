555150bb81db6c95b1fa871a6460fd4dba239589
Hugo Alvarado <hugo@britecore.com>
2020-07-25 21:48:29 -0600

    Update blog component

--- vuepress/.vuepress/components/BlogIndex.vue
+++ vuepress/.vuepress/components/BlogIndex.vue
@@ -4,8 +4,6 @@
             <h2>
                 <router-link :to="post.path">{{ post.frontmatter.title }}</router-link>
             </h2>
-
-            <p>{{ post.frontmatter.slug }}</p>
             <p>
                 <router-link :to="post.path">Read more</router-link>
             </p>
